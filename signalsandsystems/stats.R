39cd8e56aa8e5b2a57d916ae3eb61e9dacb6f33d
robbymckilliam <robbymckilliam@gmail.com>
2013-09-18 22:42:07 +0930

    minor

--- signalsandsystems/stats.R
+++ signalsandsystems/stats.R
@@ -1,8 +1,7 @@
 print("Generating markstats.pdf from marks.csv")
 marksdata <- read.csv("marks.csv")
-marks <- marksdata$Midsem.exam
+marks <- na.omit(marksdata$Midsem.exam)
 pdf(file='markstats.pdf')
-namarks <- na.omit(marks)
-hist(namarks,breaks=0:20,prob=TRUE,xlab="Marks",main=sprintf("Midsem exam, mean %.2f, median %.0f, stdev %.2f", mean(namarks), median(namarks), sd(namarks)))
-lines(density(namarks,bw="SJ"))
+hist(marks,breaks=0:20,prob=TRUE,xlab="Marks",main=sprintf("Midsem exam, mean %.2f, median %.0f, stdev %.2f", mean(marks), median(marks), sd(marks)))
+lines(density(marks,bw="SJ"))
 dev.off()
