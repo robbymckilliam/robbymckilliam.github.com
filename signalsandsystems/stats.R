73a68983373d88cbd5b533d1df93da071d465918
robbymckilliam <robbymckilliam@gmail.com>
2013-09-18 22:39:55 +0930

    minor

--- signalsandsystems/stats.R
+++ signalsandsystems/stats.R
@@ -3,6 +3,6 @@ marksdata <- read.csv("marks.csv")
 marks <- marksdata$Midsem.exam
 pdf(file='markstats.pdf')
 namarks <- na.omit(marks)
-hist(namarks,breaks=0:20,prob=TRUE,xlab="Marks",main=sprintf("Midsem exam, mean %.2f, median %.2f, stdev %.2f", mean(namarks), median(namarks), sd(namarks)))
+hist(namarks,breaks=0:20,prob=TRUE,xlab="Marks",main=sprintf("Midsem exam, mean %.2f, median %.0f, stdev %.2f", mean(namarks), median(namarks), sd(namarks)))
 lines(density(namarks,bw="SJ"))
 dev.off()
