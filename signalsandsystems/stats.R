e3db4b3f73ea81a8f569d598ea3bfa3dca714b3b
robbymckilliam <robby.mckilliam@gmail.com>
2013-11-12 11:13:39 +1030

    added pract marks

--- signalsandsystems/stats.R
+++ signalsandsystems/stats.R
@@ -1,7 +1,18 @@
 print("Generating markstats.pdf from marks.csv")
+
 marksdata <- read.csv("marks.csv")
-marks <- na.omit(marksdata$Midsem.exam)
-pdf(file='markstats.pdf')
-hist(marks,breaks=0:21-0.5,prob=TRUE,xlab="Marks",main=sprintf("Midsem exam, mean %.2f, median %.0f, stdev %.2f", mean(marks), median(marks), sd(marks)))
-lines(density(marks,bw="SJ"))
+pdf(file='markstats.pdf', width=10, height=5)
+
+layout(matrix(c(1,2,1,2), 2, 2, byrow = TRUE))
+#layout.show(2)
+
+midsemmarks <- na.omit(marksdata$Midsem.exam)
+hist(midsemmarks,breaks=0:21-0.5,prob=TRUE,xlab="Marks",main=sprintf("Midsemester exam \n # of students %i,  mean %.2f, median %.0f, stdev %.2f", length(midsemmarks), mean(midsemmarks), median(midsemmarks), sd(midsemmarks)))
+lines(density(midsemmarks,bw="SJ"))
+
+pracmarks <- na.omit(marksdata$Practical.assignment)
+hist(pracmarks,breaks=0:21-0.5,prob=TRUE,xlab="Marks",main=sprintf("Practical assignment \n # of students %i,  mean %.2f, median %.0f, stdev %.2f",length(pracmarks), mean(pracmarks), median(pracmarks), sd(pracmarks)))
+lines(density(pracmarks,adjust=2))
+
 dev.off()
+
