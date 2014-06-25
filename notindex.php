b2bf09ac9e302fbea43804d35ea0509115b7de92
Robby McKilliam <robby.mckilliam@gmail.com>
2014-06-26 09:01:26 +1000

    added doi to carrier phase paper

--- notindex.php
+++ notindex.php
@@ -78,7 +78,7 @@ R.&nbsp;G. McKilliam.
 R.&nbsp;G.&nbsp;McKilliam, A.&nbsp;Pollok, B.&nbsp;Cowley, I.&nbsp;V.&nbsp;L. Clarkson, and B.&nbsp;G. Quinn.
  Carrier phase and amplitude estimation for phase shift keying using
   pilots and data. accepted to <em>IEEE Trans. Signal Process.</em> Jun. 2014.
-[&nbsp;<a href="http://arxiv.org/abs/1301.1760">arxiv</a>&nbsp;]
+[&nbsp;<a href="http://arxiv.org/abs/1301.1760">arxiv</a>&nbsp;<a href="http://dx.doi.org/10.1109/TSP.2014.2332976">doi</a>&nbsp;]
 <!-- [&nbsp;<a href="http://arxiv.org/abs/1301.1760">arxiv</a>&nbsp;|&nbsp;<a href="https://github.com/robbymckilliam/ComplexAmplitudeEstimationPSK">code</a>&nbsp;] -->
 </td>
 </tr>
