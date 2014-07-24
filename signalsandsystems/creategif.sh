b9ad3e8fb7525adb7e2ad6726aabe6fc05919bef
Robby McKilliam <robby.mckilliam@gmail.com>
2014-07-24 16:48:08 +0930

    added newest version of notes.

--- signalsandsystems/creategif.sh
+++
@@ -1,8 +0,0 @@
-#latex $1.tex
-#dvips -t unknown $1.dvi
-#ps2pdf $1.ps
-pdflatex --shell-escape $1.tex
-#pdfcrop $1.pdf
-convert -verbose -dispose Background -delay 50 -loop 0 -density 80 $1.pdf $1.gif
-rm $1.log
-rm $1.aux
