7bb17c88b7d450ba6daebc3a20bc0312e808b021
Robby McKilliam <robby.mckilliam@gmail.com>
2014-11-10 15:30:07 +1030

    added marks

--- signalsandsystems/2014/stats.R
+++ signalsandsystems/2014/stats.R
@@ -9,9 +9,11 @@ lines(density(data,bw="SJ"))
 }
 
 #layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE))
+layout(matrix(c(1,2), 2, 1, byrow = TRUE))
 #layout.show(2)
 
 labeledhistogram(na.omit(marksdata$Midsem.exam), "Midsemester exam", 20)
+labeledhistogram(na.omit(marksdata$Practical.assignment), "Practical assignment", 20)
 
 #midsemmarks <- na.omit(marksdata$Midsem.exam)
 #hist(midsemmarks,breaks=0:21-0.5,prob=TRUE,xlab="Marks",main=sprintf("Midsemester exam \n # of students %i,  mean %.2f, median %.0f, stdev %.2f", #length(midsemmarks), mean(midsemmarks), median(midsemmarks), sd(midsemmarks)))
